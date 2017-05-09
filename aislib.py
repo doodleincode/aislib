fb523ad6938f49470985ba95d9a31f91f2395f93
NameOfTheRose <eaneonakis@yahoo.com>
2017-05-09 19:43:53 +0300

    Message Type 5 added

--- aislib.py
+++ aislib.py
@@ -2,8 +2,7 @@
 
 """
 Simple AIS library.
-This library supports creating and decoding NMEA formatted AIS type 24 messages
-(Static Data Report).
+This library supports creating and decoding NMEA formatted AIS type 1,5,24 messages
 
 @author     Daniel Hong
 https://github.com/doodleincode/aislib
@@ -225,6 +224,93 @@ class AISPositionReportMessage(AISMessage):
         self._attrs["comm_state"] = bitstring.Bits(bin=bitstream[149:168])
             
 
+class AISStaticAndVoyageReportMessage(AISMessage):
+    def __init__(self, id=5, repeat=0, mmsi=0, ais_version=0, imo=0, callsign=0, shipname=0,
+                       shiptype=0, to_bow=0, to_stern=0, to_port=0, to_starboard=0, epfd=1,
+                       month=0, day=0, hour=24, minute=60, draught=0,
+                       destination=0, dte=0, spare=0):
+        """
+        Returns an instance of an AIS Position Report Message class
+        The parameters contain the default values, simply set the parameters
+        who's value need to change. Ex:
+            
+            aismsg = AISStaticAndVoyageReportMessage(mmsi=12345,shipname='ASIAN JADE')
+        """
+        
+        super(AISStaticAndVoyageReportMessage, self).__init__({
+                    # message_element : ["data_type", num_bits, initial_value]
+                    'id'           : ["uint", 6, id], 
+                    'repeat'       : ["uint", 2, repeat], 
+                    'mmsi'         : ["uint", 30, mmsi], 
+                    'ais_version'  : ["uint", 2, ais_version], 
+                    'imo'          : ["uint", 30, imo], 
+                    'callsign'     : ["uint", 42, AISString2Bits(callsign,length=42/6).int if type(callsign) == str else callsign], 
+                    'shipname'     : ["uint", 120, AISString2Bits(shipname,length=120/6).int if type(shipname) == str else shipname], 
+                    'shiptype'     : ["uint", 8, shiptype], 
+                    'to_bow'       : ["uint", 9, to_bow], 
+                    'to_stern'     : ["uint", 9, to_stern], 
+                    'to_port'      : ["uint", 6, to_port], 
+                    'to_starboard' : ["uint", 6, to_starboard], 
+                    'epfd'         : ["uint", 4, epfd], 
+                    'month'        : ["uint", 4, month], 
+                    'day'          : ["uint", 5, day], 
+                    'hour'         : ["uint", 5, hour], 
+                    'minute'       : ["uint", 6, minute], 
+                    'draught'      : ["uint", 8, draught], 
+                    'destination'  : ["uint", 120, AISString2Bits(destination,length=120/6).int if type(destination) == str else destination], 
+                    'dte'          : ["uint", 1, dte], 
+                    'spare'        : ["uint", 1, spare]
+                })
+    
+    def build_bitstream(self):
+        return bitstring.Bits().join([
+            self.id,
+            self.repeat,
+            self.mmsi,
+            self.ais_version,
+            self.imo,
+            self.callsign,
+            self.shipname,
+            self.shiptype,
+            self.to_bow,
+            self.to_stern,
+            self.to_port,
+            self.to_starboard,
+            self.epfd,
+            self.month,
+            self.day,
+            self.hour,
+            self.minute,
+            self.draught,
+            self.destination,
+            self.dte,
+            self.spare
+        ])
+    
+    def unpack(self, bitstream):
+        # TODO: figure out a better way to do this, but works fine for now
+        self._attrs["id"]       = bitstring.Bits(bin=bitstream[0:6])
+        self._attrs["repeat"]   = bitstring.Bits(bin=bitstream[6:8])
+        self._attrs["mmsi"]     = bitstring.Bits(bin=bitstream[8:38])
+        self._attrs["ais_version"]   = bitstring.Bits(bin=bitstream[38:40])
+        self._attrs["imo"]      = bitstring.Bits(bin=bitstream[40:70])
+        self._attrs["callsign"]      = bitstring.Bits(bin=bitstream[70:112])
+        self._attrs["shipname"]       = bitstring.Bits(bin=bitstream[112:232])
+        self._attrs["shiptype"]      = bitstring.Bits(bin=bitstream[232:240])
+        self._attrs["to_bow"]      = bitstring.Bits(bin=bitstream[240:249])
+        self._attrs["to_stern"]      = bitstring.Bits(bin=bitstream[249:258])
+        self._attrs["to_port"]  = bitstring.Bits(bin=bitstream[258:264])
+        self._attrs["to_starboard"]       = bitstring.Bits(bin=bitstream[264:270])
+        self._attrs["epfd"]      = bitstring.Bits(bin=bitstream[270:274])
+        self._attrs["month"]    = bitstring.Bits(bin=bitstream[274:278])
+        self._attrs["day"]     = bitstring.Bits(bin=bitstream[278:283])
+        self._attrs["hour"]     = bitstring.Bits(bin=bitstream[283:288])
+        self._attrs["minute"]     = bitstring.Bits(bin=bitstream[288:294])
+        self._attrs["draught"]     = bitstring.Bits(bin=bitstream[294:302])
+        self._attrs["destination"]     = bitstring.Bits(bin=bitstream[302:422])
+        self._attrs["dte"]     = bitstring.Bits(bin=bitstream[422:423])
+        self._attrs["spare"] = bitstring.Bits(bin=bitstream[423:424])
+            
 class AISStaticDataReportAMessage(AISMessage):
     def __init__(self, id=24, repeat=0, mmsi=0, partno=0, shipname=0, spare=0):
         """
@@ -232,7 +318,7 @@ class AISStaticDataReportAMessage(AISMessage):
         The parameters contain the default values, simply set the parameters
         whose values need to change. Ex:
             
-            aismsg = AISPositionReportAMessage(mmsi=12345, shipname=0)
+            aismsg = AISPositionReportAMessage(mmsi=12345, shipname='ASIAN JADE')
         """
         
         super(AISStaticDataReportAMessage, self).__init__({
@@ -241,7 +327,7 @@ class AISStaticDataReportAMessage(AISMessage):
                     'repeat'          : ["uint", 2, repeat], 
                     'mmsi'            : ["uint", 30, mmsi], 
                     'partno'          : ["uint", 2, partno], 
-                    'shipname'        : ["uint", 120, shipname], 
+                    'shipname'        : ["uint", 120, AISString2Bits(shipname,length=120/6).int if type(shipname) == str else shipname], 
                     'spare'           : ["uint", 8, spare]
                 })
     
@@ -285,10 +371,10 @@ class AISStaticDataReportBMessage(AISMessage):
                     'mmsi'            : ["uint", 30, mmsi], 
                     'partno'          : ["uint", 2, partno], 
                     'shiptype'        : ["uint", 8, shiptype], 
-                    'vendorid'        : ["uint", 18, vendorid], 
+                    'vendorid'        : ["uint", 18, AISString2Bits(vendorid,length=18/6).int if type(vendorid) == str else vendorid], 
                     'model'           : ["uint", 4, model], 
                     'serial'          : ["uint", 20, serial], 
-                    'callsign'        : ["uint", 42, callsign], 
+                    'callsign'        : ["uint", 42, AISString2Bits(callsign,length=42/6).int if type(callsign) == str else callsign], 
                     'to_bow'          : ["uint", 9, to_bow], 
                     'to_stern'        : ["uint", 9, to_stern], 
                     'to_port'         : ["uint", 6, to_port], 
@@ -371,12 +457,13 @@ class AIS(object):
         to a 6-bit boundary. This value can range from 1-5.
         """
         
-        payload = "!AIVDM,1,1,,A," + self.encode() + ',0*'
+        payLoad =  self.encode() 
+        payload = "!AIVDM,1,1,,A," + payLoad + '*'
         chksum = self.crc(payload)
         
         if invert_crc:
             chksum = ~chksum
-            
+
         return payload + "%02X" % (chksum & 0xff)
         
     def encode(self, bitstr = None):
@@ -396,17 +483,19 @@ class AIS(object):
         if bitstr == None:
             bitstr = self._ais_message.build_bitstream()
         
-        # The total AIS message is 168 bits
-        # Since we are encoding 6-bit chunks, we are looping 28 times
-        # i.e. 168 / 6 = 28
-        for i in range(0, 28):
+        # The total AIS message is len(bitstr) bits
+        # Since we are encoding 6-bit chunks, we are looping
+        #  round(len(bitstr)/ 6.) times (type 5 has 424 bits which does not divide by 6
+       #print len(bitstr),len(bitstr)/6,len(bitstr)/6.,int(round(len(bitstr)/6.))
+        for i in range(0, int(round(len(bitstr)/6.))):
             block = bitstr[curr_index:curr_offset]
             encoded_str.append(encodingchars[block.uint])
             curr_index += 6
             curr_offset += 6
+        remainingbits = len(bitstr) %6
+        fillbits = (6 -remainingbits) if remainingbits !=0 else 0
 
-        return "".join(encoded_str)
-        
+        return ("".join(encoded_str))+','+chr(ord('0')+fillbits)
     def decode(self, msg):
         """
         Decodes an AIS NMEA formatted message. Currently only supports the 
@@ -429,7 +518,7 @@ class AIS(object):
         # ...
         
         # Grap just the payload. The 6th index in the AIS message contains the payload
-        payload = msg.split(",")[5]
+        payload,fillbits = msg.split(",")[5:7]
         
         # First we will reverse the 6-bit ascii encoding to its integer equivalent
         # using our reverse encoded character map
@@ -445,8 +534,16 @@ class AIS(object):
             bits.append(int2bin6(dec[i]))
             
         bitstream = "".join(bits)
-        
-        aismsg = AISPositionReportMessage()
+        if fillbits[0] !='0':bitstream = bitstream[:-int(fillbits[0])]
+        msgId = bitstream[0:6]#;print msgId
+        if   msgId == '000001':
+             aismsg = AISPositionReportMessage()
+        elif msgId == '011000' and bitstream[38] == '0':
+             aismsg = AISStaticDataReportAMessage()
+        elif msgId == '011000' and bitstream[38] == '1':
+             aismsg = AISStaticDataReportBMessage()
+        elif msgId == '000101':
+             aismsg = AISStaticAndVoyageReportMessage()
         aismsg.unpack(bitstream)
         return aismsg
     
--- aistest.py
+++ aistest.py
@@ -3,11 +3,13 @@ import aislib
 # Tests for Message Type 1
 #
 aismsg = aislib.AISPositionReportMessage(
-    mmsi = 205344990,
+    mmsi = 237772000,
+    status = 8,
+    sog = 75,
     pa = 1,
-    lon = 2644228,
-    lat = 30737782,
-    cog = 1107,
+    lon = (25*60+00)*10000,
+    lat = (35*60+30)*10000,
+    cog = 2800,
     ts = 40,
     raim = 1,
     comm_state = 82419   
@@ -20,7 +22,7 @@ print "nav status: %d" % aismsg.get_attr("status")
 aismsg2 = ais.decode(payload)
 ais2 = aislib.AIS(aismsg2)
 payload2 = ais2.build_payload(False)
-print payload2
+assert payload ==  payload2
 print "nav status: %d" % aismsg2.get_attr("status")
 
 #print aismsg.id
@@ -33,22 +35,25 @@ print "nav status: %d" % aismsg2.get_attr("status")
 # 
 # Tests for Message Type 24 Format A
 #
-print aislib.AISString2Bits('ABC')
+del aismsg
+del aismsg2
+#print aislib.AISString2Bits('ABC')
 aismsg = aislib.AISStaticDataReportAMessage(mmsi=237772000,shipname=aislib.AISString2Bits('OF THE HIGH SEAS').int)
+aismsg = aislib.AISStaticDataReportAMessage(mmsi=237772000,shipname='OF THE HIGH SEAS')
 ais = aislib.AIS(aismsg)
 payload = ais.build_payload(False)
 print payload
 aismsg2 = ais.decode(payload)
 ais2 = aislib.AIS(aismsg2)
 payload2 = ais2.build_payload(False)
-print payload2
+assert payload ==  payload2
 
 # 
 # Tests for Message Type 24 Format B
 #
-aismsg = aislib.AISStaticDataReportBMessage(mmsi=237772000,shiptype=60,
-         vendorid=aislib.AISString2Bits('DIY',length=3).int,
-         callsign=aislib.AISString2Bits('SVXYZ',length=7).int,
+aismsg = aislib.AISStaticDataReportBMessage(mmsi=237772000,shiptype=36,
+         vendorid='DIY',
+         callsign='SVXYZ',
          to_bow=5,to_stern=5,to_port=1,to_starboard=1)
 ais = aislib.AIS(aismsg)
 payload = ais.build_payload(False)
@@ -56,4 +61,24 @@ print payload
 aismsg2 = ais.decode(payload)
 ais2 = aislib.AIS(aismsg2)
 payload2 = ais2.build_payload(False)
-print payload2
+assert payload ==  payload2
+
+# 
+# Tests for Message Type 5
+#
+print ' Tests for Message Type 5'
+aismsg = aislib.AISStaticAndVoyageReportMessage(mmsi=237772000,
+         imo=0, 
+         callsign='SVXYZ',
+         shipname='OF THE HIGH SEAS',
+         shiptype=36,
+         to_bow=5,to_stern=5,to_port=1,to_starboard=1, draught=10,
+         epfd=1, month=5, day=14, hour=20, minute=15,
+         destination='STROFADES')
+ais = aislib.AIS(aismsg)
+payload = ais.build_payload(False)
+print payload
+aismsg2 = ais.decode(payload)
+ais2 = aislib.AIS(aismsg2)
+payload2 = ais2.build_payload(False)
+assert payload ==  payload2
